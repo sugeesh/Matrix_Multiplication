395939df9472e1b43b5276f12381ce2b93df6056
sugeesh <lpdneo@gmail.com>
2017-07-22 17:03:17 +0530

    matrix3 memory allocation place changed.

--- parallel.c
+++ parallel.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-double **multiplyMatrix(int number, double **matrix1, double **matrix2);
+double **multiplyMatrix(int number, double **matrix1, double **matrix2, double **matrix3);
 
 void fillValues(int number, double **matrix1, double **matrix2);
 
+void runApp(int number);
+
+
 int main() {
 
-    omp_set_num_threads(8);
 
     srand(time(NULL));  // get random number
     for (int i = 200; i <= 2000; i=i+200) {
@@ -21,12 +23,21 @@ int main() {
 
 
 void runApp(int number) {
+    // Input matrix
     double **matrix1 = (double **) malloc(sizeof(double *) * number);
     double **matrix2 = (double **) malloc(sizeof(double *) * number);
     for (int i = 0; i < number; i++)
         matrix1[i] = (double *) malloc(sizeof(double) * number);
     for (int i = 0; i < number; i++)
         matrix2[i] = (double *) malloc(sizeof(double) * number);
+
+    // Output matrix
+    double **matrix3 = (double **) malloc(sizeof(double *) * number);
+    for (int i = 0; i < number; i++) {
+        matrix3[i] = (double *) malloc(sizeof(double) * number);
+    }
+
+    omp_set_num_threads(8);
     fflush(stdout);
     fillValues(number, matrix1, matrix2);
 
@@ -34,7 +45,7 @@ void runApp(int number) {
     double sum;
     for (int i=0;i<20;i++) {
         double startTime = omp_get_wtime();
-        double **matrix3 = multiplyMatrix(number, matrix1, matrix2);
+        multiplyMatrix(number, matrix1, matrix2, matrix3);
         double endTime = omp_get_wtime();
         sum = sum +(endTime - startTime);
     }
@@ -54,19 +65,13 @@ void fillValues(int number, double **matrix1, double **matrix2) {
 }
 
 
-double **multiplyMatrix(int number, double **matrix1, double **matrix2) {
-    double **matrix3;
-    matrix3 = (double **) malloc(sizeof(double *) * number);
-    for (int i = 0; i < number; i++) {
-        matrix3[i] = (double *) malloc(sizeof(double) * number);
-    }
-
+double **multiplyMatrix(int number, double **matrix1, double **matrix2, double **matrix3) {
     // Multiplication
-#pragma omp parallel for
+    #pragma omp parallel for
     for (int i = 0; i < number; i++) {
         for (int j = 0; j < number; j++) {
-
             double count = 0;
+
             for (int m = 0; m < number; m++) {
                 count += matrix1[i][m] * matrix2[m][j];
             }
